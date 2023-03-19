5b3996d0cd8f53c5441faecb08545d4e9355c298
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-03-19 20:40:16 +0000

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import Banner from './Banner'
-import Cart from './Cart'
-import ShoppingList from './ShoppingList'
+import Banner from './Banner';
+import Cart from './Cart';
+import ShoppingList from './ShoppingList';
 
 function App() {
 	return (
